4ec95bdce8ca23077d344df46133415e0743e9f9
mohankumarSriram <thacoolsoul@gmail.com>
2019-06-20 15:20:45 +0530

    changed site url

--- src/website-config.ts
+++ src/website-config.ts
@@ -54,7 +54,7 @@ const config: WebsiteConfig = {
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
+  siteUrl: 'https://mohankumarsriram.github.io',
   twitter: 'https://twitter.com/mo_sriram',
   showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
